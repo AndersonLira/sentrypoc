ca58b8233ef75895583c39dfb3538d2a332c5fbc
Anderson Lira <contato@andersonlira.com>
2022-01-04 16:55:43 +0000

    Version without logger. Using Sentry Handler approach

--- src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
+++ src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
@@ -1,23 +1,20 @@
 package com.fontelira.sentrypoc;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import com.andersonlira.sentry9.extension.runtime.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello")
 public class ReactiveGreetingResource {
-    
-    @Inject
-    Logger logger;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        logger.info("from quarkus logger");
+    public String hello() throws Exception {
+        LoggerFactory.getLogger(this.getClass()).error("Fake error");
+        //throw new Exception("Runtime exception sentry test");
         return "Hello Sentry!";
     }
 }
