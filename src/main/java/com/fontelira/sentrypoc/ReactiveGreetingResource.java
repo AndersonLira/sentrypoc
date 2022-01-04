b50bd481da2c4a3687cd3e913e59037fe06c1a3b
Anderson Lira <contato@andersonlira.com>
2022-01-04 09:43:10 +0000

    sentry poc with extension ok. Beta version

--- src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
+++ src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
@@ -1,25 +1,23 @@
 package com.fontelira.sentrypoc;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import io.sentry.Sentry;
+import com.andersonlira.sentry9.extension.runtime.Logger;
 
 @Path("/hello")
 public class ReactiveGreetingResource {
-    @ConfigProperty(name="sentry.dsn")
-    String dsn;
+    
+    @Inject
+    Logger logger;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        Sentry.init(dsn);
-        Sentry.capture("message Quarkus");
-        Sentry.capture(new Exception("Test exception Quarkus"));
+        logger.info("from quarkus logger");
         return "Hello Sentry!";
     }
 }
