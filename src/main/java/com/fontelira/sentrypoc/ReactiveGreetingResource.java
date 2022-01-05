d7770f9f3119f076cf9f650fc0fee28cfd3ba4c7
Anderson Lira <contato@andersonlira.com>
2022-01-05 13:32:40 +0000

    WIP: master | logs ok

--- src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
+++ src/main/java/com/fontelira/sentrypoc/ReactiveGreetingResource.java
@@ -1,19 +1,25 @@
 package com.fontelira.sentrypoc;
 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Path("/hello")
 public class ReactiveGreetingResource {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ReactiveGreetingResource.class);
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() throws Exception {
-        LoggerFactory.getLogger(this.getClass()).error("Fake error");
+        LOGGER.error("Fake error");
+        LOGGER.warn("Just an warning!");
+        LOGGER.info("Just an info!");
+        //Uncomment line 23 and comment 24 to see not uncaught behavior.
         //throw new Exception("Runtime exception sentry test");
         return "Hello Sentry!";
     }
